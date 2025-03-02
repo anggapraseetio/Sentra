e52ddc425753367fa7a64746b637335847841863
anggapraseetio <e41231567@student.polije.ac.id>
2025-03-02 22:53:22 +0700

    02-03-2025
    Membuat Tampilan beranda 1

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('frontend.index');
 });
 
