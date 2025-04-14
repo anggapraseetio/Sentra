c45af952237659bec33fae0397dfb14911b28802
Aulia153 <auliasilmi133@gmail.com>
2025-04-14 08:31:33 +0700

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -15,9 +15,7 @@
                 <ul>
                     <li><a href="#hero" class="active">Beranda</a></li>
                     <li><a href="#about">Tentang Kami</a></li>
-
                     <li><a href="#faq">FAQ</a></li>
-
                     <li><a href="#contact">Kontak</a></li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
