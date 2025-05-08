5cd80118161656ca896366417e318d60100979ec
anggapraseetio <e41231567@student.polije.ac.id>
2025-05-08 09:18:56 +0700

    dashboard 99%

--- resources/views/backend/components/footer.blade.php
+++ resources/views/backend/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container copyright text-center mt-4">
-  <p>© <span>Copyright</span> <strong class="px-1 sitename">SENTRA</strong> <span>All Rights Reserved</span></p>
+  <p>© <span>Copyright</span> <strong class="px-1 sitename">SENTRA</strong> <span>2025</span></p>
   <div class="credits">
   </div>
 </div>
--- resources/views/backend/layout/admin_layout.blade.php
+++ resources/views/backend/layout/admin_layout.blade.php
@@ -2,14 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-<<<<<<< HEAD
-    <title>Focus - Bootstrap Admin Dashboard</title>
-=======
-    <title> Dashboard </title>
->>>>>>> 138e41762027b15fa7e8fdab8505d4b528c95797
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin/assets_2/images/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets_2/vendor/owl-carousel/css/owl.carousel.min.css') }}">
@@ -19,24 +11,20 @@
     <link href="{{ asset('admin/assets_2/vendor/jqvmap/css/jqvmap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('admin/assets_2/css/style.css') }}" rel="stylesheet">
     <!-- Datatable -->
-<<<<<<< HEAD
     <link href="{{ asset('admin/assets_2/vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
-=======
-    <link href="{{ 'admin/assets_2/vendor/datatables/css/jquery.dataTables.min.css' }}" rel="stylesheet">
     <!-- DataTables + Bootstrap 5 -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
->>>>>>> 138e41762027b15fa7e8fdab8505d4b528c95797
     <!-- Styles -->
     <link href="{{ asset('admin/assets/css/lib/themify-icons.css') }}" rel="stylesheet">
     {{-- Font Awesome --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 </head>
 
 <body>
-<<<<<<< HEAD
     <div id="preloader">
         <div class="sk-three-bounce">
             <div class="sk-child sk-bounce1"></div>
@@ -47,40 +35,20 @@
     <div id="main-wrapper">
         <div class="header"></div>
         @include('backend.components.navbar')
-=======
-    <div id="main-wrapper" class="d-flex">
-
-    {{-- Sidebar --}}
-    <div class="sidebar-fixed">
->>>>>>> 138e41762027b15fa7e8fdab8505d4b528c95797
         @include('backend.components.sidebar')
-    </div>
-
-    {{-- Konten Utama --}}
-    <div class="content-wrapper flex-grow-1">
-        @include('backend.components.navbar')
-        <div class="header"></div>
         <div class="content-body">
             @yield('admin')
         </div>
         @include('backend.components.footer')
     </div>
-
-<<<<<<< HEAD
-=======
     @stack('scripts')
 
-    {{-- Font Awesome --}}
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
->>>>>>> 138e41762027b15fa7e8fdab8505d4b528c95797
     <!-- Required vendors -->
     <script src="{{ asset('admin/assets_2/vendor/global/global.min.js') }}"></script>
     <script src="{{ asset('admin/assets_2/js/quixnav-init.js') }}"></script>
     <script src="{{ asset('admin/assets_2/js/custom.min.js') }}"></script>
 
-    
+
     <!-- Chart ChartJS plugin files -->
     <script src="{{ asset('admin/assets_2/js/plugins-init/chartjs-init.js') }}"></script>
 
@@ -116,18 +84,19 @@
     <script src="{{ asset('admin/assets_2/js/plugins-init/jquery-steps-init.js') }}"></script>
 
     <!-- Datatable -->
-<<<<<<< HEAD
     <script src="{{ asset('admin/assets_2/vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('admin/assets_2/js/plugins-init/datatables.init.js') }}"></script>
-=======
-    <script src="{{ 'admin/assets_2/vendor/datatables/js/jquery.dataTables.min.js' }}"></script>
-    <script src="{{ 'admin/assets_2/js/plugins-init/datatables.init.js' }}"></script>
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
->>>>>>> 138e41762027b15fa7e8fdab8505d4b528c95797
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    @hasSection('chart')
+        @yield('chart')
+    @endif
+
 
 </body>
 
