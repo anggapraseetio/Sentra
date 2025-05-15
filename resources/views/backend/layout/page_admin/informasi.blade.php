46282e8271811b0510c0e23001809f5dc3d0ecfc
Aulia153 <auliasilmi133@gmail.com>
2025-05-15 23:36:15 +0700

    tambah title

--- resources/views/backend/layout/page_admin/informasi.blade.php
+++ resources/views/backend/layout/page_admin/informasi.blade.php
@@ -1,5 +1,8 @@
 @extends('backend.layout.admin_layout')
 @section('admin')
+
+<title>Informasi</title>
+
 <div class="container-fluid">
     <div class="row page-titles mx-0">
         <div class="col-sm-6 p-md-0">
